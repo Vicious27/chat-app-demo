b5d1a84dc1114ce421e652972b0e6589b3d21349
Raul Garcia <punkvomit21@gmail.com>
2021-07-22 09:42:24 -0400

    sets up firebase config and sdk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import './App.css';
 
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDCvDjDWBnMNsM-YsH92zvk6nRVUbU01AE",
+  authDomain: "chat-app-demo-bb11c.firebaseapp.com",
+  projectId: "chat-app-demo-bb11c",
+  storageBucket: "chat-app-demo-bb11c.appspot.com",
+  messagingSenderId: "441764721930",
+  appId: "1:441764721930:web:182cae5b3a78dea985f6f3",
+  measurementId: "G-W2X4KQJJ9X"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi! welcome to your custom template.</h1>
+
       </header>
     </div>
   );
