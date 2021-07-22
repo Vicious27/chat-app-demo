1214975f27c78c92f2fcce67263833e325cb21dd
Raul Garcia <punkvomit21@gmail.com>
2021-07-22 11:42:34 -0400

    finish chat messaged chatroom and send message functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import './App.css';
 
 import firebase from "firebase/app";
@@ -57,18 +57,47 @@ function SignOut() {
 }
 
 function ChatRoom() {
+
+  const dummy = useRef();
+
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+    setFormValue('');
+
+    dummy.current.scrollIntoView({ behaviour: 'smooth' });
+  }
+
   return (
     <>
-      <div>
+      <main>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-      </div>
 
-      <form>
+        <div ref={dummy}></div>
+
+      </main>
+
+      <form onSubmit={sendMessage}>
+
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+
+        <button type="submit">🕊</button>
 
       </form>
     </>
