daac8875906b5752a448a5c7adc820938e92f1ed
Raul Garcia <punkvomit21@gmail.com>
2021-07-22 09:16:29 -0400

    set up boiler plate for chat app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi! welcome to your custom template.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
